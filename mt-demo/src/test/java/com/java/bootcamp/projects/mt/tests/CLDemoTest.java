0a57c33592edc1d983762ecfd9bee87cdcba2cc8
Juraj Veverka <gergej123@gmail.com>
2020-06-10 22:54:11 +0200

    unit test initialization fixed

--- mt-demo/src/test/java/com/java/bootcamp/projects/mt/tests/CLDemoTest.java
+++ mt-demo/src/test/java/com/java/bootcamp/projects/mt/tests/CLDemoTest.java
@@ -2,38 +2,31 @@ package com.java.bootcamp.projects.mt.tests;
 
 import com.java.bootcamp.projects.mt.cl.CLDemo;
 import com.java.bootcamp.projects.mt.cl.CLTask;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CLDemoTest {
 
-    private static ExecutorService executorService;
-
-    @BeforeAll
-    public static void init() {
-        executorService = Executors.newSingleThreadExecutor();
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(CLDemoTest.class);
+    private static final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     @Test
     public void testCountDownLatchDemo() throws InterruptedException {
+        LOG.info("TEST TEST");
         CLDemo clDemo = new CLDemo();
         CLTask clTask = new CLTask(clDemo);
+        assertNotNull(executorService);
         executorService.submit(clTask);
         clDemo.await(10, TimeUnit.SECONDS);
         assertTrue(clDemo.isClicked());
-    }
-
-    @AfterAll
-    public static void shutdown() throws InterruptedException {
-        executorService.shutdown();
-        executorService.awaitTermination(10, TimeUnit.SECONDS);
     }
 
 }

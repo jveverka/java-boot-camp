f4e345b16fd1c812fd8d79f7960c4826345e2e96
Juraj Veverka <gergej123@gmail.com>
2020-06-10 21:02:39 +0200

    javadoc added

--- mt-demo/src/main/java/com/java/bootcamp/projects/mt/executor/DataService.java
+++ mt-demo/src/main/java/com/java/bootcamp/projects/mt/executor/DataService.java
@@ -5,8 +5,16 @@ import com.java.bootcamp.projects.mt.executor.dto.Response;
 
 import java.util.concurrent.Future;
 
+/**
+ * Asynchronous data-processing service.
+ */
 public interface DataService extends AutoCloseable {
 
+    /**
+     * Dispatch data processing request.
+     * @param request - {@link Request} for data processing asynchronous task.
+     * @return {@link Future} upcoming result.
+     */
     Future<Response> getData(Request request);
 
 }
--- mt-demo/src/main/java/com/java/bootcamp/projects/mt/threadlocal/Processor.java
+++ mt-demo/src/main/java/com/java/bootcamp/projects/mt/threadlocal/Processor.java
@@ -6,8 +6,17 @@ import com.java.bootcamp.projects.mt.threadlocal.dto.TaskContext;
 
 import java.util.concurrent.Future;
 
+/**
+ * Asynchronous data-processing service.
+ */
 public interface Processor {
 
+    /**
+     * Dispatch data processing request.
+     * @param request - {@link com.java.bootcamp.projects.mt.executor.dto.Request} for data processing asynchronous task.
+     * @param taskContext - task context to pass.
+     * @return {@link Future} upcoming result.
+     */
     Future<Response> getData(Request request, TaskContext taskContext);
 
 }
